52f84bfe6aa1a9e0072e9c256ef1ac8852a6b419
ender <enderwattsgarcia@gmail.com>
2026-02-05 23:47:12 +0100

    generate template epub list

--- lib/books/books_view.dart
+++ lib/books/books_view.dart
@@ -5,6 +5,16 @@ class BooksView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('books'));
+    return ListView(children: _buildEpubWidgetList());
   }
+}
+
+List<Widget> _buildEpubWidgetList() {
+  List<Widget> epubList = [];
+
+  for (var i = 0; i < 20; i++) {
+    String text = '$i.epub';
+    epubList.add(Center(child: Text(text)));
+  }
+  return epubList;
 }
